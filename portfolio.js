f9833e9e3cd63d5ee8d9da1cbb69059e411e3f18
Ahmed <sidouparadox7@gmail.com>
2020-11-08 14:10:01 +0100

    add file

---
+++ portfolio.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $(window).on("scroll", function () {
+      $(".progress").each(function (index, progress) {
+        var progressBar = $(progress).find(".progress-bar");
+        var progressPercent = parseInt($(progressBar).attr("data-progress"));
+        if ($(window).scrollTop() > $(progress).offset().top - 400) {
+          $(progressBar)
+            .css("width", progressPercent + "%")
+            .text(progressPercent + "%");
+        }
+      });
+    });
+  });
+
+
+  document.getElementById('scrollToTop').addEventListener('click', function(){
+         window.scrollTo(0,0);
+ });
