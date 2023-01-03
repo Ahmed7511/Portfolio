13152f4c4159adf09099929cc0a9b10d47029bdc
ahmed <sidouparadox7@gmail.com>
2023-01-03 16:05:54 +0100

    carrousel for projects

---
+++ js/custom.js
@@ -0,0 +1,49 @@
+(function ($) {
+
+  "use strict";
+
+    // COLOR MODE
+    $('.color-mode').click(function(){
+        $('.color-mode-icon').toggleClass('active')
+        $('body').toggleClass('dark-mode')
+        $('ul').css('color', '#ffffff')
+        $('p').css('color', '#ffffff')
+    })
+
+    // HEADER
+    $(".navbar").headroom();
+
+    // PROJECT CAROUSEL
+    $('.owl-carousel').owlCarousel({
+    	items: 1,
+	    loop:true,
+	    margin:10,
+	    nav:true
+	});
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.nav-link, .custom-btn-link').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      });
+    });  
+
+    // TOOLTIP
+    $('.social-links a').tooltip();
+
+})(jQuery);
+
+// scroll
+$(document).ready(function () {
+  $('#more').on('click', function(){ 
+    $(this).hide();
+  });
+});
+
+  $('#scrollToTop').on('click', function(){
+         window.scrollTo(0,0);
+ });
